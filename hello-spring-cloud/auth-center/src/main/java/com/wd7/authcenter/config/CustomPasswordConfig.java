987548a82bea1bedfcca387e855886e6116b8f29
wangde007 <wangde007@outlook.com>
2018-12-13 10:34:16 +0800

    a

--- hello-spring-cloud/auth-center/src/main/java/com/wd7/authcenter/config/CustomPasswordConfig.java
+++
@@ -1,8 +0,0 @@
-package com.wd7.authcenter.config;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CustomPasswordConfig extends BCryptPasswordEncoder {
-}
