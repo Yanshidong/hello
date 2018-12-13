6774fee1c880bda212030ab7e4a3a38d14f1ac75
wangde007 <wangde007@outlook.com>
2018-12-13 15:45:10 +0800

    cp1

---
+++ hello-spring-cloud/auth-center-cp-n1/src/main/java/com/wd7/authcentercpn1/AuthCenterCpN1Application.java
@@ -0,0 +1,14 @@
+package com.wd7.authcentercpn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCenterCpN1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCenterCpN1Application.class, args);
+	}
+
+}
+
